d847ef133742633e7728e28116d117374b3b4097
Arunakun <krho79123@edu.ucl.dk>
2026-04-06 10:11:52 -0700

    Game.cs finished

--- ProjectGenspilGroup8/Models/Game.cs
+++ ProjectGenspilGroup8/Models/Game.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 
 namespace ProjectGenspilGroup8.Models
@@ -9,7 +10,7 @@ namespace ProjectGenspilGroup8.Models
         private string _name;
         private string _genre;
         private string _numberOfPlayers;
-        private List<Stockitem> _stockItems;
+        private List<StockItem> _stockItems;
 
         // Constructor
         public Game(string name, string genre, string numberOfPlayers)
@@ -18,6 +19,19 @@ namespace ProjectGenspilGroup8.Models
             this._genre = genre;
             this._numberOfPlayers = numberOfPlayers;
             _stockItems = new List<StockItem>();
+        }
+
+        // Methods
+        public string GetName() => _name;
+        public string GetGenre() => _genre;
+        public string GetNumberOfPlayers() => _numberOfPlayers;
+        public List<StockItem> GetStockItems() => _stockItems;
+
+        public void AddStockItem(StockItem stockItem) => _stockItems.Add(stockItem);
+        public void RemoveStockItem(StockItem stockItem) => _stockItems.Remove(stockItem);
+        public int GetTotalQuantity()
+        {
+            return _stockItems.Sum(item => item.GetQuantity());
         }
     }
 }
