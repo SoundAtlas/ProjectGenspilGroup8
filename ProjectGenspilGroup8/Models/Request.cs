ea5028cba6cdf8c835d2e141e1abdeabc059a24d
Arunakun <krho79123@edu.ucl.dk>
2026-04-06 10:15:50 -0700

    Request.cs finished

--- ProjectGenspilGroup8/Models/Request.cs
+++ ProjectGenspilGroup8/Models/Request.cs
@@ -4,7 +4,25 @@ using System.Text;
 
 namespace ProjectGenspilGroup8.Models
 {
-    internal class Request
+    public class Request
     {
+        private string _customerName;
+        private string _gameName;
+        private string _status;
+
+        // Constructor
+        public Request(string customerName, string gameName, string status)
+        {
+            _customerName = customerName;
+            _gameName = gameName;
+            _status = status;
+        }
+
+        // Methods
+        public string GetCustomerName() => _customerName;
+        public string GetGameName() => _gameName;
+        public string GetStatus() => _status;
+
+        public void SetStatus(string status) => _status = status;
     }
 }
