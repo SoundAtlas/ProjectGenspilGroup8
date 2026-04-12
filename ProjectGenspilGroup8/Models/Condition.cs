0fea0161d9225ddc2c1af6ffd8fbe1d63b7cd1f9
Arunakun <krho79123@edu.ucl.dk>
2026-04-11 20:48:34 -0700

    Added a comment to the enum 'Condition.cs'

--- ProjectGenspilGroup8/Models/Condition.cs
+++ ProjectGenspilGroup8/Models/Condition.cs
@@ -2,6 +2,7 @@
 
 namespace ProjectGenspilGroup8.Models
 {
+    // Stored as string in JSON for readability and stability
     [JsonConverter(typeof(JsonStringEnumConverter))]
     public enum Condition
     {
